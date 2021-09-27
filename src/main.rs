2daa6c88162594d9112ce781d68551158ef5096b
bobby3003 <roberto3003@riseup.net>
2021-09-27 15:59:14 +0700

    query

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for \"{}\",\nin file: {}", query, filename);
 }
